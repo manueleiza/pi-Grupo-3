f17e0b1a26507bea3189b4492fb2a13ae7d5213a
franciscoMonti <fmontibelotti@udesa.edu.ar>
2025-11-17 13:42:19 -0300

    comenze con el js de el category y puse el asid para que sea con el json

---
+++ js/category.js
@@ -0,0 +1,7 @@
+let quearySearch = new URLSearchParams(location.search);
+let categoria = quearySearch.get("category")
+let tituloCategoria = document.querySelector(".titulo_categoria");
+let contenedroProductos = document.querySelector(".contenedor_productos");
+
+console.log(categoriaElegida)
+
