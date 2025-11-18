6275fce3af40b58b67a0d6dc0fea94a4b5c2542e
Manuel Cristobal <mcristobal@udesa.edu.ar>
2025-11-18 18:33:26 -0300

    Creé el saludo.js y todas sus funcionalidades, desarrollé el login.js, cambiando la logica usando else - if, y agregué a todos los html :p class saludo.

--- js/login.js
+++ js/login.js
@@ -1,39 +1,25 @@
 
-let formulario = document.querySelector ('#login_formulario');
+let formulario = document.querySelector('#login_form');
 
-formulario.addEventListener("submit", function(e){
+formulario.addEventListener("submit", function (e) {
     e.preventDefault();
     alert('Estamos viendo si funciona');
 
-    let email = document.querySelector ('#email').value;
-    let contrasenia = document.querySelector ('#contrasenia').value;
+    let email = document.querySelector('#email');
+    let contrasenia = document.querySelector('#contrasenia');
 
-    if (email.value == ""){
+    if (email.value == "") {
         alert('El campo del email es obligatorio')
-    } else {
-        this.submit()
-    }
-
-     if (contrasenia.value == ""){
+    } else if (contrasenia.value == "") {
         alert('El campo de la contraseña es obligatorio')
+    } else if (contrasenia.value.length < 6) {
+        alert('La contraseña debe tener al menos 6 caracteres');
     } else {
+        localStorage.setItem('emailUser', email.value)
+        let emailGuardado = localStorage.getItem('emailUser');
+        console.log('Email:', emailGuardado);
+
         this.submit()
     }
-
-    if (contrasenia.length < 6) {
-    alert('La contraseña debe tener al menos 6 caracteres');
-       } else {
-        this.submit()
-    }
-
-
-localStorage.setItem('userEmail', email)
-
-let emailUsuario = localStorage.getItem('userEmail');
-console.log(emailUsuario);
-
-
-
-// ME FALTA AGREGAR Y QUE VUELVA A LA PAGINA DE INICIO SI ES QUE ES VALIDO. 
-
 });
+
---
+++ js/saludo.js
@@ -0,0 +1,35 @@
+let emailAlmacenado = localStorage.getItem('emailUser');
+
+let lista = document.querySelector('.listaHeader');
+let saludo = document.querySelector('.saludo');
+
+
+
+if (emailAlmacenado){
+
+    saludo.innerText = `Bienvenido: ${emailAlmacenado}`;
+    
+    lista.innerHTML =
+     ` <li><a href="#">${emailAlmacenado}</a></li>
+        <li>|</li>
+        <li><a href="./login.html" id="logout">Logout</a></li>`;
+
+        let logout = document.querySelector('#logout');
+        logout.addEventListener('click', function(){
+            localStorage.removeItem('emailUser');
+        })
+
+    }else{
+  alert("No se guardaron bien los datos");
+
+
+
+}
+
+
+
+
+
+
+
+
