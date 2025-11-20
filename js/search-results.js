7cb78f01e5d94e6b11ea0c2a997784a1334fc0a4
franciscoMonti <fmontibelotti@udesa.edu.ar>
2025-11-20 12:26:35 -0300

    Comenze con el js del serch result

---
+++ js/search-results.js
@@ -0,0 +1,33 @@
+let quearySearch = new URLSearchParams(location.search);
+let producto_buscado = quearySearch.get("buscador")
+let mas_vendidos = document.querySelector(".contenedor-busqueda");
+
+fetch(`https://dummyjson.com/products/search?q=${producto_buscado}`)
+    .then(function (respuesta) {
+        return respuesta.json()
+    })
+    .then(function (listaProductos) {
+        
+        let productos = listaProductos.products;
+
+        if (productos.length > 0) {
+
+            for (let i = 0; i < productos.length; i++) {
+
+                let producto = `
+                    <article class="articulo_producto">
+                        <img src="${productos[i].images[0]}"></img>
+                        <h3>${productos[i].title}</h3>
+                        <p>${productos[i].description}</p>
+                        <p>$${productos[i].price}</p>
+                        <a href="./product.html?detalles=${productos[i].title}">Ver Detalles</a>
+                    </article>`;
+
+                mas_vendidos.innerHTML += producto;
+            }
+
+        } else {
+
+            mas_vendidos.innerHTML = `<h1>No hay resultados para el término: ${producto_buscado}</h1>`
+        }
+    });
